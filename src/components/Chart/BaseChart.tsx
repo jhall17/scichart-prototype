a8c8842b647103a400b8e8b8623f1a0729c91c7e
John Hall <johngradyh@gmail.com>
2023-07-17 17:53:19 -0400

    eod monday, progress on theme, overview

--- src/components/Chart/BaseChart.tsx
+++ src/components/Chart/BaseChart.tsx
@@ -1,30 +1,46 @@
 import { useRef, useEffect, useState } from "react";
-import { I2DSurfaceOptions, SciChartSurface } from "scichart";
+import {
+  I2DSurfaceOptions,
+  IThemeProvider,
+  SciChartJSDarkv2Theme,
+  SciChartOverview,
+  SciChartSurface,
+} from "scichart";
 import { DrawFunction } from "./Bob2d";
+import { createCustomTheme } from "./chart.theme";
 
 type BaseChartProps = {
   draw: DrawFunction;
   surfaceOptions?: I2DSurfaceOptions;
   divStyle?: React.HTMLProps<HTMLDivElement>;
+  overviewDiv?: React.RefObject<HTMLDivElement>;
 };
 
-const BaseChart = ({ draw, surfaceOptions, divStyle }: BaseChartProps) => {
+const BaseChart = ({
+  draw,
+  surfaceOptions = {},
+  divStyle = {},
+  overviewDiv,
+}: BaseChartProps) => {
   const [curSurface, setCurSurface] = useState<SciChartSurface>();
   const ref = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     (async () => {
       // TODO: how do we handle this default
-      const res = await SciChartSurface.create(
-        ref?.current ?? "",
-        surfaceOptions
-      );
+      const res = await SciChartSurface.create(ref?.current ?? "", {
+        ...surfaceOptions,
+      });
       const { surface } = draw(res.sciChartSurface, res.wasmContext);
       setCurSurface(surface);
     })();
 
+    overviewDiv &&
+      curSurface &&
+      SciChartOverview.create(curSurface, overviewDiv.current ?? "");
+
     return () => curSurface?.delete();
-  }, [draw, surfaceOptions]);
+  }, [draw]);
 
   return (
     <div ref={ref} style={{ height: "100%", width: "100%", ...divStyle }} />
---
+++ src/components/Chart/chart.theme.ts
@@ -0,0 +1,66 @@
+import { IThemeProvider, SciChartJSDarkv2Theme } from "scichart";
+
+export function createCustomTheme(
+  overrides: Partial<IThemeProvider> = {}
+): IThemeProvider {
+  // Create a custom theme by implementing all the properties from IThemeProvider
+  const customTheme = {
+    axisBorder: "Transparent",
+    axisTitleColor: "#6495ED",
+    annotationsGripsBackroundBrush: "white",
+    annotationsGripsBorderBrush: "white",
+    axis3DBandsFill: "#1F3D6833",
+    axisBandsFill: "#1F3D6833",
+    axisPlaneBackgroundFill: "Transparent",
+    chartTitleColor: "#6495ED",
+    columnFillBrush: "white",
+    columnLineColor: "white",
+    cursorLineBrush: "#6495ED99",
+    defaultColorMapBrush: [
+      { offset: 0, color: "DarkBlue" },
+      { offset: 0.5, color: "CornflowerBlue" },
+      { offset: 1, color: "#FF22AA" },
+    ],
+    downBandSeriesFillColor: "#52CC5490",
+    downBandSeriesLineColor: "#E26565FF",
+    downBodyBrush: "white",
+    downWickColor: "white",
+    fillPalette: ["white", "green", "blue", "purple"],
+    gridBackgroundBrush: "white",
+    gridBorderBrush: "white",
+    labelBackgroundBrush: "#6495EDAA",
+    labelBorderBrush: "#6495ED",
+    labelForegroundBrush: "#EEEEEE",
+    legendBackgroundBrush: "#1D2C35",
+    lineSeriesColor: "white",
+    loadingAnimationBackground: "#0D213A",
+    loadingAnimationForeground: "#6495ED",
+    majorGridLineBrush: "#1F3D68",
+    minorGridLineBrush: "#102A47",
+    mountainAreaBrush: "white",
+    mountainLineColor: "white",
+    overviewFillBrush: "white",
+    planeBorderColor: "white",
+    rolloverLineBrush: "#FD9F2533",
+    rubberBandFillBrush: "#99999933",
+    rubberBandStrokeBrush: "#99999977",
+    sciChartBackground: "#0D213A",
+    scrollbarBackgroundBrush: "white",
+    scrollbarBorderBrush: "white",
+    scrollbarGripsBackgroundBrush: "white",
+    scrollbarViewportBackgroundBrush: "white",
+    scrollbarViewportBorderBrush: "white",
+    strokePalette: ["purple", "blue", "green", "white"],
+    shadowEffectColor: "white",
+    textAnnotationBackground: "#6495EDAA",
+    textAnnotationForeground: "#EEEEEE",
+    tickTextBrush: "#6495ED",
+    upBandSeriesFillColor: "white",
+    upBandSeriesLineColor: "white",
+    upBodyBrush: "#6495EDA0",
+    upWickColor: "#6495ED",
+    ...overrides,
+  };
+
+  return customTheme;
+}
